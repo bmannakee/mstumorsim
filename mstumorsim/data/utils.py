478646f2f5d96bb9b491d2bc9da28bd12d908ccb
Brian Mannakee <brianmannakee@on-campus-10-138-72-47.vpn.arizona.edu>
2018-08-14 16:19:16 -0700

    add get_spectrum and test

---
+++ mstumorsim/data/utils.py
@@ -0,0 +1,19 @@
+import os
+import pandas
+
+this_path,this_file = os.path.split(__file__)
+SIG_FILE = os.path.join(this_path,'data','signature_probabilities.txt')
+
+def load_signatures():
+    sigs = pandas.read_table(SIG_FILE)
+    sigs.columns = sigs.columns.str.replace(" ","_")
+    return sigs
+
+def get_spectrum(sigs):
+    sigs = load_signatures()
+    cols = [f'Signature_{sig}' for sig in sigs]
+    spec_fr = sigs[cols]
+    spec = spec_fr.sum(axis=0)
+    spec = spec/sum(spec)
+    return(spec)
+
--- mstumorsim/tests/test_tumorsim.py
+++ mstumorsim/tests/test_tumorsim.py
@@ -2,11 +2,21 @@
 
 import unittest
 
+import mstumorsim.utils
 from mstumorsim.tumorsim import SNVtree
+
 
 class TestTumorSim(unittest.TestCase):
     def test_basic_sim(self):
         spec = [1/96. for x in range(95)]
         spec.append(1-sum(spec))
         tree = SNVtree(100,spec)
+        print(f'Number of cells is {len(tree.get_cells)}')
         self.assertEqual(len(tree.get_cells),100)
+
+    def test_spectrum(unittest.TestCase):
+        spec = utils.get_spectrum([1,5])
+        print(f'Length of spectrum is {len(spec)}')
+        print(f'Sum of spectrum is {sum(spec)}')
+        self.assertEqual(len(spec),96)
+        self.assertEqual(sum(spec),1)
