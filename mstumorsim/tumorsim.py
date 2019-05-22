7693727c8d70c3c9d0419553f20a0942a9e98958
bmannakee <mannakee@email.arizona.edu>
2019-05-22 16:21:16 -0700

    check in the major refactor

--- mstumorsim/tumorsim.py
+++ mstumorsim/tumorsim.py
@@ -20,15 +20,16 @@ class Mutation:
         # multinomial returns an array with a 1 in the bin that holds the returned value
         # and 0 everywhere else. 1-indexed for further processing.
         self.mutation_class = np.argmax(multinomial.rvs(1,spectrum)) + 1
+    
+    def __repr__(self):
+        return("mutation {} of type {}".format(self.id,self.mutation_class))
 
 
 class Cell:
        # TODO: add bozic parameters Should mutations be their own object. With ID and spectrum? '''
-        def __init__(self,mut_rate,parent,is_empty=False,spectrum=None,mutations=None, seq_type = 'exome'):
-            self.is_empty = is_empty
+        def __init__(self,parent,mu = 3e-6, spectrum = None, mutations = None, seq_type = 'exome'):
             self.rep_rate = 2 # Cells just divide
-            self.dormant = False
-            self.mr = mut_rate
+            self.mu = mu
             self.seq_type = seq_type
             if mutations == None:
                 self.mutations = []
@@ -38,45 +39,20 @@ class Cell:
             self.parent_id = parent
             self.spectrum = spectrum # Each cell has a spectrum and division generates mutations from that spectrum
 
-        def reproduce(self,new_sigs=None, force=False):
+        def reproduce(self,new_sigs=None):
          # This is where we reproduce TODO: add bozic parameters '''
-           # a d/b ratio of .72 implies 72 deaths per 100
-           # births. bernoulli(.58) gets that. 
-           # force = True forces reproduction, useful for starting new tree
-            daughters = [] # Daughters has to be an iterable even if empty
+            daughters = [] 
             if self.seq_type == "exome":
-                #num_muts = bernoulli.rvs(size=1,p=0.015)[0]
-                num_muts = 9 
+                num_muts = 3e7 * self.mu 
             else:
-                num_muts = 900
-            if self.is_empty:
-                ## Generating an empty tree
-                if force:
-                    rep = 1
-                else:
-                    rep = bernoulli.rvs(size=1,p=0.51)
-
-                if (rep==1 and not self.dormant):
-                    for i in range(self.rep_rate):
-                        daughter = Cell(mut_rate = self.mr,parent = self.id, is_empty = True, seq_type = self.seq_type)
-                        daughters.append(daughter)
-            else:
+                num_muts = 3e9 * self.mu
                 ## Not generating an empty tree
-                tmp_spec = Spectrum(new_sigs)
-                if (np.array_equal(tmp_spec.sigs,self.spectrum.sigs) and not force):
-                    #rep = bernoulli.rvs(size=1,p=0.51) #  d/1-d=.72 gives, b=.58, d=.42
-                    rep=1
-                else:
-                    # if sigs change, force at least the initial reproduction
-                    rep = 1
-                if (rep==1 and not self.dormant):
-                    for i in range(self.rep_rate):
-                        current_mutations = copy.deepcopy(self.mutations)
-                        current_mutations.extend([Mutation(tmp_spec.spectrum) for x in list(range(num_muts))])
-                        daughter = Cell(mut_rate = self.mr,parent = self.id,is_empty = False, spectrum = tmp_spec,mutations =current_mutations)
-                        daughters.append(daughter)
-                else:
-                    self.dormant = True # Safety issues to work out here. should be private?
+            tmp_spec = Spectrum(new_sigs)
+            for i in range(self.rep_rate):
+                current_mutations = copy.deepcopy(self.mutations)
+                current_mutations.extend([Mutation(tmp_spec.spectrum) for x in list(range(int(num_muts)))])
+                daughter = Cell(mu = self.mu,parent = self.id, spectrum = tmp_spec,mutations =current_mutations)
+                daughters.append(daughter)
             return(daughters)
         
         def get_mutations(self):
@@ -87,14 +63,18 @@ class Cell:
             
         def get_sigs(self):
             return self.spectrum.sigs
+        
+        def __repr__(self):
+            return("A {} with signatures {} and mutations {}".format(self.__class__.__name__,self.spectrum,[mutation for mutation in self.get_mutations()]))
+
 
 class SNVtree:
    # A tree that does stuff. More documentation please '''
 
-    def __init__(self,num_cells,empty=False,sigs=None,timepoints=None,seq_type='exome'):
+    def __init__(self, num_cells = 2, mu = 3e-6, sigs = None, timepoints = None, seq_type = 'exome'):
         '''
         num_cells: The total number of cells in the tree at completion
-        empty: Bool - True to create a tree of empty cells, False to simulate cells with mutations.
+        mu: The point mutation rate
         sigs: List of integers (1-30) specifying the COSMIC mutational signatures to include in the tumor
         timepoints: List of floats the same length as "sigs" at which the signature should become active.
                     timepoint 0 for signatures present at initiation, .25 for sigs appearing after 25% of
@@ -102,88 +82,41 @@ class SNVtree:
         seq_type: one of "exome" or "wgs". Determines the mutations selected and the number of mutations
                     per cell division
         '''
-        self.make_empty = empty
-        if not self.make_empty:
-            self.input_sigs = np.array(sigs)
-            self.input_timepoints = np.array(timepoints)
-            self.init_sigs = self.input_sigs[np.where(self.input_timepoints == 0.)]
-            self.init_spectrum = Spectrum(self.init_sigs)
-            self.add_sigs = self.input_sigs[np.where(self.input_timepoints > 0.)]
-            self.add_timepoints = self.input_timepoints[np.where(self.input_timepoints > 0.)]
-            self.next_timepoint_index = 0
-            self.seq_type = seq_type
-        else:
-            self.input_sigs = None
-            self.input_timepoints = None
-            self.init_sigs = None
-            self.init_spectrum = None
-            self.add_sigs = None
-            self.add_timepoints = None
-            self.next_timepoint_index = None
+        self.mu = mu
+        self.input_sigs = np.array(sigs)
+        # Begin with current sigs = input sigs.
+        # When signatures change they change for the whole tree, not just the cell.
+        # Doing it for a single cell will likely be nearly invisible, but it is possible.
+        self.current_sigs = self.input_sigs
+        self.input_timepoints = np.array(timepoints)
+        self.init_sigs = self.input_sigs[np.where(self.input_timepoints == 0.)]
+        self.init_spectrum = Spectrum(self.init_sigs)
+        self.add_sigs = self.input_sigs[np.where(self.input_timepoints > 0.)]
+        self.add_timepoints = self.input_timepoints[np.where(self.input_timepoints > 0.)]
+        self.next_timepoint_index = 0
+        self.seq_type = seq_type
         self.n = num_cells
+        if self.seq_type == "exome":
+            num_muts = 3e7 * self.mu 
+        else:
+            num_muts = 3e9 * self.mu 
         self.queue = deque()
         # Begin with a single cell with parent = 1. during the run force the first 100 cells to reproduce
-        if self.make_empty:
-            initial_cell = [Cell(mut_rate=2,parent=1,is_empty=True)]
-        else:
-            initial_cell = [Cell(seq_type = self.seq_type, mut_rate = 9, parent = 1,spectrum = self.init_spectrum, \
-                mutations = [Mutation(self.init_spectrum.spectrum),Mutation(self.init_spectrum.spectrum)])] 
+        initial_cell = [Cell(mu = self.mu, seq_type = self.seq_type, parent = 1,spectrum = self.init_spectrum, \
+            mutations = [Mutation(self.init_spectrum.spectrum) for x in list(range(int(num_muts)))])] 
                             
         self.queue.extend(initial_cell)
 
 
 
     def run(self):
-        if self.make_empty:
-            while len(self.queue) < 10:
-                # Get the tree started with 10 cells that are gauranteed to reproduce
-                c = self.queue.popleft()
-                if c.dormant:
-                    self.queue.append(c) 
-                    continue
-                else:
-                    new_cells = c.reproduce(force=True)
-                    #self.queue.append(c) # NO!. Don't put it back! It divided and is gone!
-                    self.queue.extend(new_cells)
-
-            while len(self.queue) < self.n:
-                c = self.queue.popleft()
-                if c.dormant:
-                    self.queue.append(c)   
-                else:
-                    new_cells = c.reproduce()
-                    #self.queue.append(c) # NO!. Don't put it back! It divided and is gone!
-                    self.queue.extend(new_cells)
-        else:
-            while len(self.queue) < 4:
-                # Get the tree started with 4 cells that are gauranteed to reproduce
-                c = self.queue.popleft()
-                if c.dormant:
-                    self.queue.append(c)
-                    continue
-                else:
-                    new_sigs = c.get_sigs()
-                    new_cells = c.reproduce(new_sigs = new_sigs, force = True)
-                    #self.queue.append(c) # NO!. Don't put it back! It divided and is gone!
-                    self.queue.extend(new_cells)
-                # Get the tree started with 4 cells that are gauranteed to reproduce
-            while len(self.queue) < self.n:
-                if 98 < len(self.queue) < 100:
-                    print('made it to 100')
-                c = self.queue.popleft()
-                # test for dormancy here to speed things up
-                if c.dormant:   
-                    self.queue.append(c)
-                    continue
-                if self._test_for_new_spectrum(len(self.queue)):
-                    # Awkward, we already incremented timepoint index
-                    new_sigs = np.append(c.get_sigs(),self.add_sigs[self.next_timepoint_index - 1])  
-                else:
-                    new_sigs = c.get_sigs()
-                #new_cells = c.reproduce(new_sigs = new_sigs, force = False)
-                new_cells = c.reproduce(new_sigs = new_sigs, force = True) # for faster simulation without evolution, force replication
-                #self.queue.append(c) # Push current cell back onto the end of the queue. If cell is marked dormant it will never reproduce
-                self.queue.extend(new_cells)
+        while len(self.queue) < self.n:
+            c = self.queue.popleft()
+            if self._test_for_new_spectrum(len(self.queue)):
+                # Awkward, we already incremented timepoint index
+                self.current_sigs = np.append(c.get_sigs(),self.add_sigs[self.next_timepoint_index - 1])  
+            new_cells = c.reproduce(new_sigs = c.get_sigs())
+            self.queue.extend(new_cells)
 
 
     def get_cells(self):
@@ -212,21 +145,12 @@ class SNVtree:
         mcount = []
         num_cells = len(self.get_cells())
         for k,v in mc.items():
-            vaf = float(v)/num_cells
+            vaf = float(v)/(2*num_cells)
             if vaf < min_vaf:
                 continue
             mcount.append((k,vaf))
         return mcount
 
-    def get_graph(self):
-        g = nx.DiGraph()
-        g.add_node(1,)
-        nodes = self.get_cells()
-        for node in nodes:
-            g.add_node(node.id,cell=node)
-        edges = [(node.parent_id,node.id) for node in nodes]
-        g.add_edges_from(edges)
-        return(g)
 
     def make_bed(self, chr_style = "UCSC",min_vaf = 0.01, from_pickle = True):
         # Gather all of the mutations from this tree that
@@ -259,6 +183,7 @@ class SNVtree:
         # The sampling scheme is not guaranteed to produce unique mutations
         # Dropping is not the best answer
         print("NOT! dropping duplicates, don't use this with bamsurgeon")
+        #print("dropping duplicates")
         #fr.drop_duplicates(inplace = True)
         return(fr)
     
@@ -285,3 +210,6 @@ class SNVtree:
             return True
         else:
             return False 
+    
+    def __repr__(self):
+        return(cell for cell in self.get_cells())
